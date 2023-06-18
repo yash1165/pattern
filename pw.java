5b0e15a6976b5bb556befb33194c4bc3513048f8
yash1165 <yashsawant1165@gmail.com>
2023-06-18 10:30:36 +0530

    third change

--- pw.java
+++ pw.java
@@ -188,6 +188,25 @@ class HelloWorld
             }
              System.out.println();
             } 
+            
+            for(int i=0; i<n; i++)
+            {
+             for(int j=0; j<n; j++)
+
+            {
+                if(j==0 || i==0 || j==n-1 || i==n-1 || i+j<=4 || j-i>=4    )
+                {
+                    System.out.print("* ");
+
+                    
+                }
+                else
+                {
+                    System.out.print("  ");
+                }
+            }
+             System.out.println();
+            } 
 
 
 
